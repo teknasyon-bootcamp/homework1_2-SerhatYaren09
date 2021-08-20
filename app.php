4401dd35f38e82f8f742cba893c538614ef01390
Serhat YAREN <64776783+SerhatYaren09@users.noreply.github.com>
2021-08-20 16:44:19 +0300

    Hw2_answer

--- app.php
+++ app.php
@@ -1,11 +1,3 @@
-<?php
-
-$gender = "male"; // cinsiyet (male/female)
-$weight = 55; // kilo (kg)
-$height = 170; // boy (cm)
-$age = 25; // yaş (sene)
-
-$guess = "1470"; // Tahmin edilen değer
 
 /**
  * Bu ödevde bazal metabolizma hesaplaması yapacağız.
@@ -34,3 +26,45 @@ $guess = "1470"; // Tahmin edilen değer
  * 
  * yazması gerekiyor.
  */
+
+
+
+<?php  
+
+                       $gender = "male"; // cinsiyet (male/female)
+                       $weight = 55; // kilo (kg)
+                       $height = 170; // boy (cm)
+                       $age = 25; // yaş (sene)
+
+                       $guess = "1470"; // Tahmin edilen değer
+
+                        if ($gender=="male") {
+                            $bmr_calc = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
+                        }
+                          else {
+
+                            $bmr_calc = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
+                          }  
+                       
+                       echo "BMR:" . $bmr_calc;
+                       echo "<br>";
+
+                       if ($bmr_calc > $guess) {
+                            
+                              echo "Tahmin değerinden büyük";                           
+  
+                       }
+                       
+                       elseif ($bmr_calc == $guess) {
+                              
+                              echo "Tahmin değerine eşit";
+                       }
+
+                       else
+                       {
+                             echo "Tahmin değerinden düşük";
+                       }
+
+                       
+
+       ?>
